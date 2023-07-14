ff56a11774d6c6bf552d63ce33614584a13e3ed3
sanjay <manikantasanjay1999@gmail.com>
2023-07-14 17:14:24 +0530

    updated-client3-py-file

--- client3.py
+++ client3.py
@@ -35,14 +35,17 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2  # Calculate the average price
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    """Get the ratio of price_a and price_b"""
+    if price_b == 0:
+        return None  # Handle division by zero error
+    return price_a / price_b
 
 
 # Main
@@ -51,9 +54,18 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        prices = []
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices.append(price)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        if len(prices) == 2:
+            price_a, price_b = prices  # Assuming there are only two stocks in quotes
+            ratio = getRatio(price_a, price_b)
+            if ratio is not None:
+                print("Ratio %s" % ratio)
+            else:
+                print("Invalid ratio due to division by zero")
+        else:
+            print("Insufficient quotes to calculate ratio")
